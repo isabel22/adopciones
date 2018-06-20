6f521b1ab0f22d09059ee3081034544fc0b88f2f
Isabel Garcia <isabelgg22@gmail.com>
2018-06-20 13:58:05 +0200

    adds CRUD for animal

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,41 @@
+class AnimalsController < ApplicationController
+
+  def index
+    @animals = Animal.order(created_at: :desc)
+  end
+
+  def show
+    puts "params::: #{params}"
+    @animal = Animal.find(params[:animal_id])
+  end
+
+  def delete
+    animal = Animal.find(params[:animal_id])
+    animal.destroy
+
+    redirect_to(animals_path, alert: "Animal deleted")
+  end
+
+  def edit
+    @animal = Animal.find(params[:animal_id])
+  end
+
+  def update
+    animal = Animal.find(params[:animal_id])
+    animal.update!(safe_params)
+
+    redirect_to(animals_path, notice: "Animal updated")
+  end
+
+  def create
+    Animal.create!(safe_params)
+
+    redirect_to(animals_path, notice: "Animal created")
+  end
+
+  private
+
+  def safe_params
+    params.require(:animal).permit!
+  end
+end
