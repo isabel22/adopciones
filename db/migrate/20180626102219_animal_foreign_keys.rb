37c09cf2d1adb2eec62d6d766beaabdc7242e6b7
Isabel Garcia <isabelgg22@gmail.com>
2018-06-26 14:52:33 +0200

    add foreign keys for animal_breeds on animals

---
+++ db/migrate/20180626102219_animal_foreign_keys.rb
@@ -0,0 +1,5 @@
+class AnimalForeignKeys < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :animals, :animal_breeds, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_25_121307) do
+ActiveRecord::Schema.define(version: 2018_06_26_102219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2018_06_25_121307) do
     t.date "adoption_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "animal_breeds_id"
+    t.index ["animal_breeds_id"], name: "index_animals_on_animal_breeds_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,4 +60,5 @@ ActiveRecord::Schema.define(version: 2018_06_25_121307) do
   end
 
   add_foreign_key "animal_breeds", "animal_species", column: "animal_species_id"
+  add_foreign_key "animals", "animal_breeds", column: "animal_breeds_id"
 end
