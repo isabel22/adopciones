fa3f3c17425a1d8e46cedcc40561489080138d1b
Isabel Garcia <isabelgg22@gmail.com>
2018-07-11 13:53:25 +0200

    wip 2 request form

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -10,9 +10,15 @@ class RequestsController < ApplicationController
 
   def new
     @request = Request.new
+    animal_id = params[:animal_id]
+    animal = Animal.find(animal_id)
+    @other_types = animal.other_types
   end
 
   def edit
+    animal_id = params[:animal_id]
+    animal = Animal.find(animal_id)
+    @other_types = animal.other_types
   end
 
   def create
@@ -41,11 +47,12 @@ class RequestsController < ApplicationController
   end
 
   private
-    def set_request
-      @request = Request.find(params[:request_id])
-    end
 
-    def request_params
-      params.fetch(:request, {})
-    end
+  def set_request
+    @request = Request.find(params[:request_id])
+  end
+
+  def request_params
+    params.fetch(:request, {})
+  end
 end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,4 +2,10 @@ class Animal < ApplicationRecord
   # TODO: add different status instead of only set adopted / have control of
   # the animals history.
   has_one :animal_breed
+
+  def other_types
+    breed = AnimalBreed.find(self.animal_breeds_id)
+    species_id = AnimalSpecy.find(breed.animal_species_id).id
+    AnimalSpecy.where.not(id: species_id).pluck(:id, :name)
+  end
 end
