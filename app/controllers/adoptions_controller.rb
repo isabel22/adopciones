069f60a49db6d97775eda4a3428dccde399f8983
Isabel Garcia <isabelgg22@gmail.com>
2018-07-12 18:56:32 +0200

    adds frontpage with animals to adopt

---
+++ app/controllers/adoptions_controller.rb
@@ -0,0 +1,7 @@
+class AdoptionsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @animals = Animal.where(adopted: false).order(created_at: :desc)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'root#index'
+  root 'adoptions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :animals, only: [:index, :new] do
     get :show
