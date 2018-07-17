8de927d096d02d8180f50ccc8e7cb07b7c177213
Isabel Garcia <isabelgg22@gmail.com>
2018-07-17 14:33:55 +0200

    populates  page for requests

--- spec/views/requests/show.html.erb_spec.rb
+++ spec/views/requests/show.html.erb_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "requests/show", type: :view do
   before(:each) do
-    @request = assign(:request, FactoryBot.create(:request))
+    @animal = FactoryBot.create(:animal)
+    @request = assign(:request, FactoryBot.create(:request, animal_id: @animal.id))
+    @other_types = assign(:other_types, @animal.other_types)
   end
 
   it "renders attributes in <p>" do
