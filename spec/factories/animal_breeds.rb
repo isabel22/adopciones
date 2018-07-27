10962182abea774077774a07a9d966434955f110
Isabel Garcia <isabelgg22@gmail.com>
2018-07-27 12:49:56 +0200

    adds validations for animal breeds

--- spec/factories/animal_breeds.rb
+++ spec/factories/animal_breeds.rb
@@ -2,6 +2,6 @@
 FactoryBot.define do
   factory :animal_breed do
     sequence(:name) {|n| "Breed #{n}" }
-    animal_species_id { FactoryBot.create(:animal_specy).id }
+    animal_specy_id { FactoryBot.create(:animal_specy).id }
   end
 end
