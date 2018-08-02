81a25be51284e4f8cb18e9dfc6ed09477d53abbd
Isabel Garcia <isabelgg22@gmail.com>
2018-08-02 18:20:47 +0200

    sets default role when creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   rolify
+  after_create :set_default_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,5 +8,11 @@ class User < ApplicationRecord
 
   def can_be_removed?
     Request.where(email: self.email).count == 0
+  end
+
+  private
+
+  def set_default_role
+    self.add_role(:requester) if self.roles.blank?
   end
 end
