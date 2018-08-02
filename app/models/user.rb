6900f5d755ec637b629db66fba039b629619a177
Isabel Garcia <isabelgg22@gmail.com>
2018-08-02 18:06:28 +0200

    updates styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def can_be_removed?
+    Request.where(email: self.email).count == 0
+  end
 end
