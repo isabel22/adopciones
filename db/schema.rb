93a518b7c79977a8e49e0805f7ffc0cd23d8b908
Isabel Garcia <isabelgg22@gmail.com>
2021-04-13 19:20:51 +0200

    add the description field to the animal model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_195041) do
+ActiveRecord::Schema.define(version: 2021_04_12_174809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_08_16_195041) do
     t.datetime "updated_at", null: false
     t.bigint "animal_breeds_id"
     t.string "profile_picture"
+    t.string "description"
     t.index ["animal_breeds_id"], name: "index_animals_on_animal_breeds_id"
   end
 
