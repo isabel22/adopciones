a6b4fb3fd1f90199eb957259cf6eebccfe555b9e
Isabel Garcia <isabelgg22@gmail.com>
2021-04-18 14:41:34 +0200

    fix rubocop

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -63,15 +63,8 @@ class AnimalsController < ApplicationController
 
   def safe_params
     params.require(:animal).permit(
-      :name,
-      :gender,
-      :birthdate_rescuedate,
-      :rescued,
-      :adopted,
-      :adoption_date,
-      :animal_breeds_id,
-      :profile_picture,
-      :description
+      :name, :gender, :birthdate_rescuedate, :rescued, :adopted,
+      :adoption_date, :animal_breeds_id, :profile_picture, :description
     )
   end
 end
